59bd67f4da20234efc6052f8bd07da81e2ce8ce3
Gonzalo <gmoreno@acid.cl>
2014-03-24 16:21:18 -0400

    add locales and missing views

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -3,10 +3,6 @@ module Spree
     def edit
       @payment = @order.payments.order(:id).last
 
-      redirect_to webpay_success_path(@payment.token) and return if @payment and @payment.token? and ['processing', 'completed'].include?(@payment.state)
-
-      redirect_to webpay_error_path(@payment.token) and return if @payment and @payment.token? and @payment.failed? and params[:state] != 'payment'
-
       if params[:state] == Spree::Gateway::WebpayPlus.STATE and @order.state == Spree::Gateway::WebpayPlus.STATE
         payment_method     = @order.payment_method
 
@@ -17,22 +13,11 @@ module Spree
         failure_url        = webpay_failure_url(:protocol => "http")
         provider = payment_method.provider.new
         response = provider.pay(amount, @order.number, trx_id, success_url, failure_url)
+        
         respond_to do |format|
           format.html { render text: response }
         end
-        # if response.success?
-        #   # TODO - ver si se puede reutilizar el token cuando este ya esta seteado
-        #   @payment.update_attributes token: response.get_token
-        #   redirect_to response.payment_process_url
-
-        #   # To clean the Cart
-        #   session[:order_id] = nil
-        #   @current_order     = nil
-
-        #   return
-        # else
-        #   @error = response.get_error
-        # end
+        
       end
     end
   end
