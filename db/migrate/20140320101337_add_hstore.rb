296dd6a61b0ee3b7cce50c0f9c5a2fa6c6fd5b0e
Ignacio Verdejo <iverdejo@acid.cl>
2014-05-16 17:39:13 +0000

    Update migration to add hstore extension

--- db/migrate/20140320101337_add_hstore.rb
+++ db/migrate/20140320101337_add_hstore.rb
@@ -1,9 +1,9 @@
-class AddHstoreExtension < ActiveRecord::Migration
+class AddHstore < ActiveRecord::Migration
   def up
-    execute 'CREATE EXTENSION IF NOT EXISTS hstore'
+    enable_extension 'hstore'
   end
 
   def down
-    execute 'DROP EXTENSION hstore'
+    disable_extension 'hstore'
   end
 end
