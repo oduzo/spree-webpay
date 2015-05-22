2944c68ce68cbcbf94832263288b338fb0c85d0d
Alexander Sysoev <alexander@acid.cl>
2015-05-22 12:20:48 -0300

    Add additional condition to migration
    In order to avoid migration conflicts.

--- db/migrate/20150520181448_add_accepted_column_to_spree_payment.rb
+++ db/migrate/20150520181448_add_accepted_column_to_spree_payment.rb
@@ -1,6 +1,6 @@
 class AddAcceptedColumnToSpreePayment < ActiveRecord::Migration
   def change
-    add_column :spree_payments, :accepted, :boolean
+    add_column(:spree_payments, :accepted, :boolean) unless Spree::Payment.column_names.include?('accepted')
 
     unless index_exists?(:spree_payments, :webpay_trx_id)
       add_index :spree_payments, :webpay_trx_id
